61c8a8d5f87b15136a05d36c1921314bc6415fbc
Misael Virissimo De Moura <porincrivelqpareca@gmail.com>
2017-06-28 12:31:38 -0400

    temporary staged

--- pindrop-app/src/App.js
+++ pindrop-app/src/App.js
@@ -2,18 +2,78 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: "hotpink"
+    }
+  }
+
+  toggleColor() {
+    if(this.state.color === "hotpink"){
+      this.setState({
+        color: "yellow"
+      });
+    } else {
+      this.setState({
+        color: "hotpink"
+      });
+    }    
+  }
+
+  changeColor(event) {
+    this.setState({
+      color: event.target.value
+    });
+  }
+
   render() {
+    const styleObj = {
+      background: this.state.color
+    }
+    console.log(this)
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+        <div className="col-md-12">
+            <h1> Submit an Event </h1>
+            <form action="" method="post">
+                <div className="form-group">                        
+                    <h3>Title:</h3>
+
+                    <input className="form-control" type="text" for="title" name="title" required />
+                </div>
+                <div className="form-group"> 
+                    <h3 >Description:</h3>
+
+                    <textarea className="form-control"  name="description"></textarea>
+                </div>
+                <div className="form-group"> 
+                    <h3 >Category:</h3>
+                    <select className="form-control" name="category">
+                        <option value="meetup">Meetup</option>
+                        <option value="conference">Conference</option>
+                        <option value="lecture">Lecture</option>
+                    </select>
+                </div>      
+                <button className="btn btn-block btn-primary" type="submit">Post Event</button>
+            </form>            
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+
+
+
+
+      //<section style={styleObj} for="hello-world">
+       // <h2 onClick={this.toggleColor.bind(this)}>{this.state.color}</h2>  
+       // <input value={this.state.color} onChange={this.changeColor.bind(this)}/>  
+       // <h2 className={}>{this.props.name}</h2>
+      // </section>
     );
   }
 }
