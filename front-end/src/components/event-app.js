6bc9096aafc05c714173c88fe0138424cfe944f8
Kevin McGuinness <2kevinmcg@gmail.com>
2017-07-06 21:12:16 -0400

    Add event-app.js

---
+++ front-end/src/components/event-app.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+
+class event-app extends Component {
+    render() {
+        return (
+            /* Author: Kevin McGuinness */
+            <h1>Basic Event Information</h1>
+            <div className="event-basics">
+                <h3>Event Name: Defending Against PHP Pirates</h3>
+                /* Code to get and display database field */
+            </div>
+            <div className="event-basics">
+                <h3>Event Date: August 10, 2017</h3>
+                /* Code to get and display database field */
+            </div>
+            <div className="event-basics">
+                <h3>Start Time: 6:00 p.m.</h3>
+                /* Code to get and display database field */
+            </div>
+            <div className="event-basics">
+                <h3>End Time: 8:00 p.m.</h3>
+                /* Code to get and display database field */
+            </div>
+            <div className="event-basics">
+                <h3>Group Holding Event: Data Defenders Consortium</h3>
+                /* Code to get and display database field */
+            </div>
+            <div className="event-basics">
+                <h3>Location: 226 South Pearl Street, Albany, NY 12203</h3>
+                /* Code to get and display database field */
+            </div>
+        );
+    }
+}
+
+export default event-app;
